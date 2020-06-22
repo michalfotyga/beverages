b5d86d6eb6a1352678fabf3509362ac43e029a7e
Michal Fotyga <michal.fotyga@gmail.com>
2020-06-22 13:21:58 +0200

    add junit version + add countByName

--- src/main/java/BeverageService.java
+++ src/main/java/BeverageService.java
@@ -1,5 +1,10 @@
 import java.util.List;
+import java.util.Map;
 
+//Carlsberg Pilsner Heineken Skol Pilsner Corona
 public interface BeverageService {
+    
     String sortedBeverages(List<String> beverages);
+    
+    Map<String, Integer> countByName(List<String> beverages);
 }
